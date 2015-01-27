40a30f8955ec13f31339372d6f0edb80c712d32d
Eugene M <eugene@getbetter.com>
2015-01-26 20:04:57 -0800

    Fixed some resizing behavior.

--- fixmenu.js
+++ fixmenu.js
@@ -1,13 +1,39 @@
 jQuery("document").ready(function($){
 	
+		if($( window ).width() <580) {
+			$('.media').addClass('media-resized');
+		}
+		else {
+			$('.media').removeClass('media-resized');
+		}
+
 	var nav = $('.menu');
 	
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 5) {
 			nav.removeClass("space");
 		} else {
-			nav.removeClass("space");
+			nav.addClass("space");
 		}
 	});
+	
+
+	$( window ).resize(function() {
+		if($( window ).width() <770) {
+			$('.media').addClass('media-resized1');
+		}
+		else {
+			$('.media').removeClass('media-resized1');
+		}		
+		if($( window ).width() <580) {
+			$('.media').addClass('media-resized2');
+			$('.topbar').addClass('topbar-resize');
+		}
+		else {
+			$('.media').removeClass('media-resized2');
+			$('.topbar').removeClass('topbar-resize');
+		}
+	});
+	
  
 });
