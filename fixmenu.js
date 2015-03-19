3628b6dd5b0a6dae083714739e95da3144e5cd46
Eugene M <eugene@getbetter.com>
2015-03-19 15:59:49 -0700

    Sexy succulent smooth scrolling

--- fixmenu.js
+++ fixmenu.js
@@ -1,5 +1,35 @@
 jQuery("document").ready(function($){
 
+	var scrollVal = 80
+
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+	        || location.hostname == this.hostname) {
+
+	        var target = $(this.hash);
+	        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	           if (target.length) {
+	             $('html,body').animate({
+	                 scrollTop: target.offset().top - scrollVal
+	            }, 1000);
+	            return false;
+	        }
+	    }
+
+	});
+ 	// $(".scroll").click(function(event){
+  //        event.preventDefault();
+  //        //calculate destination place
+  //        var dest=0;
+  //        if($(this.hash).offset().top > $(document).height()-$(window).height()){
+  //             dest=$(document).height()-$(window).height();
+  //        }else{
+  //             dest=$(this.hash).offset().top;
+  //        }
+  //        //go to destination
+  //        $('html,body').animate({scrollTop:dest}, 1000,'swing');
+  //    });
+
 		if($( window ).width() >900) {
 			$('.navbar-right').addClass('topbar1');
 		}		
