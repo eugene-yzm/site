36405790fc849d4e10cf35da88f2da02a657b3d6
Eugene M <eugene@getbetter.com>
2015-03-19 10:18:06 -0700

    Kreygasm resizing

--- fixmenu.js
+++ fixmenu.js
@@ -1,12 +1,5 @@
 jQuery("document").ready(function($){
 
-		if($( window ).width() <580) {
-			$('.media').addClass('media-resized');
-		}
-		else {
-			$('.media').removeClass('media-resized');
-		}
-
 		if($( window ).width() >900) {
 			$('.navbar-right').addClass('topbar1');
 		}		
@@ -26,30 +19,38 @@ jQuery("document").ready(function($){
 		}		
 
 		if($( window ).width() <700) {
-			$('.navbar-right').addClass('topbar4');
-			$('.navbar-right').removeClass('topbar3');
+			$('.navbar-right').removeClass('topbar4');
+			$('.middle').addClass('w650');
+			$('.topbar').addClass('w631');
 		}	
+
 	$(window).resize(function() {
 		if($( window ).width() <1600) {
-			$('.navbar-right').removeClass('topbar3');
-			$('.navbar-right').addClass('topbar2');
+			$('.navbar-right').removeClass('topbar4');
+			$('.navbar-right').addClass('topbar3');
 
 			if($( window ).width() <1400) {
-				$('.navbar-right').removeClass('topbar2');
-				$('.navbar-right').addClass('topbar1');
+				$('.navbar-right').removeClass('topbar3');
+				$('.navbar-right').addClass('topbar2');
 
 				if($( window ).width() <700) {
-					$('.middle').width('650px');
-				}					
+					$('.middle').addClass('w650');
+					$('.topbar').addClass('w631');
+				}
+
+				if($( window ).width() >700) {
+					$('.middle').removeClass('w650');
+					$('.topbar').removeClass('w631');
+				}		
 			}	
 			else{
-				$('.navbar-right').addClass('topbar2');
-				$('.navbar-right').removeClass('topbar1');				
+				$('.navbar-right').addClass('topbar3');
+				$('.navbar-right').removeClass('topbar2');				
 			}			
 		}	
 		else {
-			$('.navbar-right').addClass('topbar3');
-			$('.navbar-right').removeClass('topbar2');			
+			$('.navbar-right').addClass('topbar4');
+			$('.navbar-right').removeClass('topbar3');			
 		}
 	});
 });
